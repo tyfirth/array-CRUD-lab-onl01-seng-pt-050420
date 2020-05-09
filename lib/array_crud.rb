35a65412c30ab81b554030894b6c5d899c0475f9
Tyler Firth <tyfirth@gmail.com>
2020-05-09 03:23:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   myArray = [1,2,3,4]
 end
 
-def add_element_to_end_of_array(myArray, n)
-  myArray << 5
+def add_element_to_end_of_array(array. element)
+  array << 5
 end
 
 def add_element_to_start_of_array(array, element)
