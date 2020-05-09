30890c05ac9111a40001cd48f4c385aa5271d0e3
Tyler Firth <tyfirth@gmail.com>
2020-05-09 02:11:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  fourArray.push(5)
+  newArray = [1,2,3]
+  newArray << 4
 end
 
 def add_element_to_start_of_array(array, element)
