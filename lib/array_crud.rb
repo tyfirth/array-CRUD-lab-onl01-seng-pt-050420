777e53e830591e0b844f5f489af11acae913e701
Tyler Firth <tyfirth@gmail.com>
2020-05-09 03:32:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def add_element_to_end_of_array(myArray, element)
   myArray << element
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(myArray, element)
   myArray.unshift(element)
 end
 
